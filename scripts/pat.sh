6f172be110bc7d309cc58f44e477715bcde0cd36
Kamesh Sampath <kamesh.sampath@hotmail.com>
2025-08-05 10:02:32 +0530

    (fix): Make it idempotent

--- scripts/pat.sh
+++ scripts/pat.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euo pipefail 
 
-## Setup Service User for Cortex Agent
+## Setup Service User for Openflow
 snow sql --stdin <<EOF
     use role accountadmin;
     CREATE USER IF NOT EXISTS $SA_USER
         TYPE = SERVICE
-        COMMENT = 'Service User For Cortex Agents MCP Demo';
-    GRANT ROLE $SNOWFLAKE_MCP_DEMO_ROLE TO USER $SA_USER;
+        COMMENT = 'Service User For Openflow';
+    GRANT ROLE $SNOWFLAKE_OPENFLOW_DEMO_ROLE TO USER $SA_USER;
+    GRANT APPLY AUTHENTICATION POLICY ON USER $SA_USER TO ROLE $SNOWFLAKE_OPENFLOW_DEMO_ROLE;
 EOF
 
 # # Get GitHub Actions IP ranges only IPV4
@@ -24,8 +25,8 @@ CIDR_VALUE_LIST="'${LOCAL_IP}'"
 # Create or alter the network rule and policy
 snow sql --query "use role accountadmin;alter user $SA_USER unset network_policy;" || true
 snow sql --stdin <<EOF
-use role $SNOWFLAKE_MCP_DEMO_ROLE;
-use database $SNOWFLAKE_MCP_DEMO_DATABASE;
+use role $SNOWFLAKE_OPENFLOW_DEMO_ROLE;
+use database $SNOWFLAKE_OPENFLOW_DEMO_DATABASE;
 create schema if not exists networks;
 create schema if not exists policies;
 
@@ -33,53 +34,57 @@ use role accountadmin;
 
 -- unset and drop the network policy on the user
 alter user "${SA_USER}" unset network_policy;
-drop network policy if exists LOCAL_PAT_NETWORK_POLICY;
-use role $SNOWFLAKE_MCP_DEMO_ROLE;
+drop network policy if exists "${SA_USER}_LOCAL_PAT_NETWORK_POLICY";
+use role $SNOWFLAKE_OPENFLOW_DEMO_ROLE;
 -- create or replace network rule to allow local machine IPv4 addresses
-create or replace network rule $SNOWFLAKE_MCP_DEMO_DATABASE.networks.pat_local_access_rule
+create or replace network rule $SNOWFLAKE_OPENFLOW_DEMO_DATABASE.networks.pat_local_access_rule
   mode = ingress
   type = ipv4
   value_list = ($CIDR_VALUE_LIST)
   comment = 'Allow only local machine IPv4 addresses';
 -- attach the network rule to the network policy
 use role accountadmin;
-create network policy LOCAL_PAT_NETWORK_POLICY
-allowed_network_rule_list = ('"$SNOWFLAKE_MCP_DEMO_DATABASE".networks.pat_local_access_rule')
+create network policy "${SA_USER}_LOCAL_PAT_NETWORK_POLICY"
+allowed_network_rule_list = ('"$SNOWFLAKE_OPENFLOW_DEMO_DATABASE".networks.pat_local_access_rule')
 comment = 'Network policy to allow all IPv4 addresses.';
-alter user "${SA_USER}" set network_policy='LOCAL_PAT_NETWORK_POLICY';
+alter user "${SA_USER}" set network_policy="${SA_USER}_LOCAL_PAT_NETWORK_POLICY";
 EOF
 
 # Create or alter the authentication policy that will be set to the service user
 snow sql --query "use role accountadmin;alter user $SA_USER unset AUTHENTICATION POLICY;" || true
 snow sql --stdin <<EOF
-use role $SNOWFLAKE_MCP_DEMO_ROLE;
-create or replace authentication policy $SNOWFLAKE_MCP_DEMO_DATABASE.policies."${USER}_mcp_auth_policy"
+use role $SNOWFLAKE_OPENFLOW_DEMO_ROLE;
+GRANT USAGE ON DATABASE $SNOWFLAKE_OPENFLOW_DEMO_DATABASE TO ROLE ACCOUNTADMIN;
+GRANT USAGE ON SCHEMA $SNOWFLAKE_OPENFLOW_DEMO_DATABASE.policies TO ROLE ACCOUNTADMIN;
+GRANT USAGE ON SCHEMA $SNOWFLAKE_OPENFLOW_DEMO_DATABASE.networks TO ROLE ACCOUNTADMIN;
+create or replace authentication policy $SNOWFLAKE_OPENFLOW_DEMO_DATABASE.policies.${USER}_openflow_auth_policy
   authentication_methods = ('PROGRAMMATIC_ACCESS_TOKEN')
   pat_policy = (
     default_expiry_in_days=15,
     max_expiry_in_days=30,
     network_policy_evaluation = ENFORCED_REQUIRED
   );
- alter user $SA_USER set AUTHENTICATION POLICY $SNOWFLAKE_MCP_DEMO_DATABASE.policies."${USER}_mcp_auth_policy";
+  alter user $SA_USER set AUTHENTICATION POLICY $SNOWFLAKE_OPENFLOW_DEMO_DATABASE.policies.${USER}_openflow_auth_policy;
 EOF
 
 # Create PAT for the service user
 # Check if PAT already exists
-EXISTING_PAT=$( snow sql -q "show pats for user $SA_USER" \
+EXISTING_PAT=$( snow sql -q "show user pats for user $SA_USER" \
   --format=json \
-  | jq -r '.[] | select(.name|ascii_downcase == "mcp_demo") | .name')
+  | jq -r '.[] | select(.name|ascii_downcase == "openflow_demo") | .name')
 
 if [ -z "$EXISTING_PAT" ]; then
   # Create PAT if it doesn't exist
   echo "Creating new PAT for service user $SA_USER..."
-  SNOWFLAKE_SA_PASSWORD=$(snow sql \
-    --query "ALTER USER IF EXISTS $SA_USER ADD PAT mcp_demo ROLE_RESTRICTION = $SNOWFLAKE_MCP_DEMO_ROLE" \
+  SNOWFLAKE_SA_PASSWORD=$(snow sql --role="accountadmin" \
+    --query "ALTER USER IF EXISTS $SA_USER ADD PAT openflow_demo ROLE_RESTRICTION = $SNOWFLAKE_OPENFLOW_DEMO_ROLE" \
     --format=json | jq -r '.[] | .token_secret')
+  printf "PAT:\n%s" "$SNOWFLAKE_SA_PASSWORD"
 else
   echo "PAT for service user $SA_USER already exists. Rotating PAT..."
-  # Rotate PAT
+  # Rotate PAT 
   SNOWFLAKE_SA_PASSWORD=$(snow sql \
-    --query "ALTER USER IF EXISTS $SA_USER ROTATE PAT mcp_demo" \
+    --query "ALTER USER IF EXISTS $SA_USER ROTATE PAT openflow_demo" \
     --format=json | jq -r '.[] | .token_secret')
 fi;
 
